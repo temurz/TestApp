dad4ccef15708634a502f9d6673609d9d3ff6cc3
Temur <timurzaitov1998@gmail.com>
2023-05-14 14:32:52 +0200

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Temur on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
